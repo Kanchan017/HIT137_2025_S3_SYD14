a2e7c03d4e2766df520c57a901e19a5c55a7abb3
Kanchan Lamichhane <lamichhanek986@gmail.com>
2026-01-10 19:02:15 +1100

    Add decryption logic, write to decrypted.txt and verification.

--- Assignment_2/question_1.py
+++ Assignment_2/question_1.py
@@ -48,3 +48,59 @@ encrypted_text = encrypt(rw_txt, shift_1, shift_2)
 
 with open("encrypted_text.txt", "w") as f:
     f.write(encrypted_text)
+
+
+#DECRYPTION
+# Exactly opposite logic of encryption for decryption.
+def decrypt(encrypted_text, shift_1, shift_2):
+    decrypted_text = ""
+
+    for char in encrypted_text:
+        if 'a' <= char<='m':
+                shift = shift_1 * shift_2
+                new_ord = chr((ord(char) - ord('a') - shift)%13 + ord('a') )           
+                decrypted_text += new_ord
+        
+        #second half of alphabet:(n-z)
+        elif 'n' <= char <= 'z':
+                shift = shift_1 + shift_2
+                new_ord = chr((ord(char) - ord('n') + shift)%13 + ord('n') ) 
+                decrypted_text += new_ord          
+            
+        #uppercase
+        elif  'A' <= char <='M':
+               shift = shift_1
+               new_ord = chr((ord(char) - ord('A') + shift)%13 + ord('A') )           
+               decrypted_text += new_ord
+            
+        elif 'N' <= char <= 'Z':
+                shift = shift_1 * shift_2
+                new_ord = chr((ord(char) - ord('N') - shift)%13 + ord('N') )           
+                decrypted_text += new_ord
+
+        #for other characters
+        else:
+            decrypted_text += char
+
+    return decrypted_text
+
+with open("encrypted_text.txt", "r") as f:
+    encrypted_text = f.read()
+
+decrypted_text =decrypt(encrypted_text, shift_1, shift_2)
+
+with open("decrypted_text.txt","w") as f:
+    f.write(decrypted_text)
+
+
+#Verification
+if rw_txt ==decrypted_text:
+    print("Decryption successful!")
+else:
+    print("Decryption failed!")
+
+
+
+
+
+    
