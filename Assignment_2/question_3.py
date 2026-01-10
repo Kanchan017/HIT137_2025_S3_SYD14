07339756506a8a960f4590766d2001951fabb492
abhaacharya13 <69753197+abhaacharya13@users.noreply.github.com>
2026-01-10 21:26:26 +0545

    Implemention  Draw regular polygon with straight edges only [Depth 0]

---
+++ Assignment_2/question_3.py
@@ -0,0 +1,22 @@
+# Using a recursive function to generate a geometric pattern using Python's turtle graphics
+
+import turtle as t
+import math
+t.speed(0)
+t.hideturtle()
+#Defining recursive function
+def draw_o(leng,depth):
+    #Case: if depth =0 draw a straight line
+    if depth == 0:
+        t.forward(leng)
+   
+s = int(input("Please enter the number of sides: "))
+s_length = int(input("Pleae enter the side length: "))
+depth = int(input("Please enter the recursion depth: "))
+
+angle = 360/sides
+
+for i in range(s):
+    draw_o(s_length,0)#for depth 0
+    t.left(angle)   
+t.done()
